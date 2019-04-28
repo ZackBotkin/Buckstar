e54751eee6c3806a6c85acd0904bb28efe3e967e
Zack <zackbotkin@gmail.com>
2019-04-28 14:28:04 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+class Stock:
+
+    name = 'Apple'
+    price = 0.0
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+
+
+class Account:
+
+    stocks = []
+    balance = 0.0
+
+    def __init__(self, balance):
+        self.balance = balance
+
+
+    def add_stock(self, stock):
+        stocks.add(stock)
+
+
+
+def main():
+    print("Ok")
+
+
+
+if __name__ == '__main__':
+    main()
